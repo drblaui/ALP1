f2083cd3f4974ce6e1f66a9de9423185cb1f9d53
Drblau <drblaui@users.noreply.github.com>
2019-11-04 13:17:14 +0100

    updated so miss esponda isn't gonna beat me with an old stick

--- Übung2/ue02_print.hs
+++ Übung2/ue02_print.hs
@@ -16,23 +16,53 @@ factorial n
         | n < 0 = error "not defined for negative numbers"
 
 
-euler :: Float -> Float
+euler :: Double -> Double
 euler n 
        | n == 0 = 1
        | otherwise = (1 / factorial(n)) + euler(n-1)
 
 
 --Aufgabe 4
-insertInPos :: Char -> Int -> [Char] -> [Char]
+--TODO: Make recurisve
+{-
+Ich möchte nur mal anmerken, dass diese Funktion hier nicht nur viel einfacher, sondern laufzeiteffektiver wäre:
 insertInPos char offset xs
             | ((offset < 0) || (offset > length xs )) = error "This offset is not reachable inside the list"
             | (offset == 0) = char : xs
-            | otherwise =((fst(splitAt(offset) xs)) ++ (char : (snd(splitAt(offset) xs))))
+            | otherwise = ((fst(splitAt(offset) xs)) ++ (char : (snd(splitAt(offset) xs))))
+-}
+insertInPos :: Char -> Int -> [Char] -> [Char]
+insertInPos char offset xs 
+            | ((offset < 0) || (offset > length xs )) = error "This offset is not reachable inside the list"
+            | (offset == 0) = char : xs
+            | otherwise = insertInPosHelper [head xs] (tail xs) char offset
 
+insertInPosHelper :: [Char] -> [Char] -> Char -> Int -> [Char]
+insertInPosHelper xs ys char offset
+                | (length xs == offset) = xs ++ (char:ys)
+                | otherwise = insertInPosHelper (xs ++ [head ys]) (tail ys) char offset
 --Aufgabe 5
 trueDivisor :: Int -> [Int]
 -- Aint list comprehension somethin beautiful 
 trueDivisor n = [a | a <- [1..(n-1)], n `mod` a == 0]
 
 --Aufgabe 6
---What the fuck?
+--a
+negation :: Int -> Int
+negation n = -(n - 1)
+
+und :: Int -> Int -> Int
+und a b = a * b
+
+oder :: Int -> Int -> Int
+oder a b = negation(und (negation a) (negation b))
+
+exoder :: Int -> Int -> Int
+exoder a b = oder(und (negation a) b) (und a (negation(b)))
+
+--b
+hamming_distance :: [Int] -> [Int] -> Int
+hamming_distance [] [] = 0
+hamming_distance _ [] = error "Bit-Lists differ in length"
+hamming_distance [] _ = error "Bit-Lists differ in length"
+hamming_distance (x:xs) (y:ys) = exoder x y + hamming_distance xs ys
